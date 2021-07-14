4b440d204f348490c1e906b60d698c7f69a2ab55
SuKhingThan-Partner <sukhingthan.sbs@gmail.com>
2021-07-14 16:32:16 +0630

    addData

---
+++ lib/image_detail.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+import 'dart:io';
+import 'dart:ui';
+import 'dart:async';
+
+class DetailScreen extends StatefulWidget {
+  final String imagePath;
+  DetailScreen(this.imagePath);
+
+  @override
+  _DetailScreenState createState() => new _DetailScreenState(imagePath);
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  _DetailScreenState(this.path);
+
+  final String path;
+
+  Size _imageSize;
+  List<TextElement> _elements = [];
+  String recognizedText = "Loading ...";
+
+  void _initializeVision() async {
+    final File imageFile = File(path);
+
+    if (imageFile != null) {
+      await _getImageSize(imageFile);
+    }
+
+    final FirebaseVisionImage visionImage =
+        FirebaseVisionImage.fromFile(imageFile);
+
+    final TextRecognizer textRecognizer =
+        FirebaseVision.instance.textRecognizer();
+
+    final VisionText visionText =
+        await textRecognizer.processImage(visionImage);
+
+    String pattern =
+        r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$";
+  //  RegExp regEx = RegExp(pattern);
+
+    String mailAddress = "";
+    for (TextBlock block in visionText.blocks) {
+      for (TextLine line in block.lines) {
+        mailAddress += line.text + '\n';
+        for (TextElement element in line.elements) {
+          _elements.add(element);
+        }
+        // if (regEx.hasMatch(line.text)) {
+        //   mailAddress += line.text + '\n';
+        //   for (TextElement element in line.elements) {
+        //     _elements.add(element);
+        //   }
+        // }
+      }
+    }
+
+    if (this.mounted) {
+      setState(() {
+        recognizedText = mailAddress;
+      });
+    }
+  }
+
+  Future<void> _getImageSize(File imageFile) async {
+    final Completer<Size> completer = Completer<Size>();
+
+    final Image image = Image.file(imageFile);
+    image.image.resolve(const ImageConfiguration()).addListener(
+      ImageStreamListener((ImageInfo info, bool _) {
+        completer.complete(Size(
+          info.image.width.toDouble(),
+          info.image.height.toDouble(),
+        ));
+      }),
+    );
+
+    final Size imageSize = await completer.future;
+    setState(() {
+      _imageSize = imageSize;
+    });
+  }
+
+  @override
+  void initState() {
+    _initializeVision();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Image Details"),
+      ),
+      body: _imageSize != null
+          ? SingleChildScrollView(
+                              child: Container(
+                                padding: EdgeInsets.all(8.0),
+                                child: Text(
+                                  recognizedText,
+                                  style: TextStyle(
+                                    fontSize: 18.0
+                                  ),
+                                ),
+                              )
+                            )
+
+
+          : Container(
+              color: Colors.black,
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+    );
+  }
+}
+
+class TextDetectorPainter extends CustomPainter {
+  TextDetectorPainter(this.absoluteImageSize, this.elements);
+
+  final Size absoluteImageSize;
+  final List<TextElement> elements;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final double scaleX = size.width / absoluteImageSize.width;
+    final double scaleY = size.height / absoluteImageSize.height;
+
+    Rect scaleRect(TextContainer container) {
+      return Rect.fromLTRB(
+        container.boundingBox.left * scaleX,
+        container.boundingBox.top * scaleY,
+        container.boundingBox.right * scaleX,
+        container.boundingBox.bottom * scaleY,
+      );
+    }
+
+    final Paint paint = Paint()
+      ..style = PaintingStyle.stroke
+      ..color = Colors.red
+      ..strokeWidth = 2.0;
+
+    for (TextElement element in elements) {
+      canvas.drawRect(scaleRect(element), paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(TextDetectorPainter oldDelegate) {
+    return true;
+  }
+}
